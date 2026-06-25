42c3738bbe87433eb6ebc35db95a1ae2dce13463
julinhodavan <lalalulu1wo@gmail.com>
2026-06-25 08:06:05 -0300

    Adicionado cargo e salario ao Funcionario

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -4,10 +4,14 @@ class Funcionario
 {
     private $nome;
     private $matricula;
+    private $cargo;
+    private $salario;
 
-    public function __construct($nome, $matricula)
+    public function __construct($nome, $matricula, $cargo, $salario)
     {
         $this->nome = $nome;
         $this->matricula = $matricula;
+        $this->cargo = $cargo;
+        $this->salario = $salario;
     }
 }
